6ff70008a9055f4a3060f2cfb025df92943f3eb7
HPHuerta <hhernanp@gmail.com>
2024-08-16 01:35:57 -0400

    feat: Add scraper-grocery category data scraping functionality

--- scraper-grocery/groceryScraper.js
+++ scraper-grocery/groceryScraper.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs')
 
 async function scrapeWebsite(url) {
   // Launch the browser 
@@ -8,17 +9,13 @@ async function scrapeWebsite(url) {
   const page = await browser.newPage()
 
     // navigate to the URL 
-    await page.goto(url, {
-      waitUntil: 'networkidle2'
-    }) 
-
-    await page.waitForSelector('.css-1uwjmhu')
+    await page.goto(url) 
     
     const cardsScrape = await page.evaluate(() => {
-      const icons = Array.from(document.querySelectorAll('.css-1uwjmhu'))
+      const icons = Array.from(document.querySelectorAll('.category'))
       const data = icons.map((el) => ({
         image: el.querySelector('img').getAttribute('src'),
-        title: el.querySelector('p').innerText
+        title: el.querySelector('h3 a').innerText
       }))
 
       return data
@@ -29,6 +26,11 @@ async function scrapeWebsite(url) {
     // close the browser
     await browser.close()
 
+    fs.writeFile('categories.json', JSON.stringify(cardsScrape), (err) => {
+      if(err) throw err
+      console.log("JSON successfully created");
+    })
+
 }
 
-scrapeWebsite('https://www.zehrs.ca/')
+scrapeWebsite('https://www.saveonfoods.com/sm/planning/rsid/1982/')
