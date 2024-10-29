dd58d905b133df1c22222a9ce61342e26ad46e1b
HPHuerta <hhernanp@gmail.com>
2024-10-28 23:58:11 -0400

    feat: Add CatsController with CRUD operations and CreateCatDto

--- nestjs/nest-practice/src/cats.controller.ts
+++ nestjs/nest-practice/src/cats.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Res,
+  Post,
+  Param,
+  Body,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    console.log(createCatDto);
+    return 'This action adds a new cat';
+  }
+
   @Get()
-  findAll() {
-    return 'This action returns all cats';
+  findAll(@Res({ passthrough: true }) res: Response) {
+    res.status(HttpStatus.OK).send('Test');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    console.log(id);
+    return `This action returns a #${id} cat`;
   }
 }
---
+++ nestjs/nest-practice/src/create-cat.dto.ts
@@ -0,0 +1,12 @@
+export class CreateCatDto {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+export class ListAllEntities {
+  name: string;
+  age: number;
+  breed: string;
+  limit: 2;
+}
