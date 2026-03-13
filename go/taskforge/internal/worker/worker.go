acec0a8b92a74e18cf893109ea8f6183b1ab4959
HPHuerta <hhernanp@gmail.com>
2026-03-12 23:53:38 -0400

    refactor job workers

--- go/taskforge/internal/worker/worker.go
+++ go/taskforge/internal/worker/worker.go
@@ -1,33 +1,60 @@
 package worker
 
 import (
-	"fmt"
 	"log"
+	"time"
 )
+
+type JobType string
+
+const (
+	JobTaskCompleted JobType = "task_completed"
+)
+
+type Job struct {
+	Type     JobType
+	TaskID   int
+	Message  string
+	CreateAt time.Time
+}
 
 type Processor struct {
 	// This is a channel of strings
-	jobs chan string
+	jobs chan Job
 }
 
 func NewProcessor(buffer int) *Processor {
 	return &Processor{
 		// this create a buffered channel. buffer is the jobs can be queued
-		jobs: make(chan string, buffer),
+		jobs: make(chan Job, buffer),
 	}
 }
 
 func (p *Processor) Start() {
 	go func() {
 		for job := range p.jobs {
-			log.Printf("processing job: %s\n", job)
-			fmt.Printf("background job done: %s\n", job)
+			log.Printf(
+				"processing job type=%s task_id=%d created_at=%s message=%q",
+				job.Type,
+				job.TaskID,
+				job.CreateAt.Format(time.RFC3339),
+				job.Message,
+			)
+
+			// Simulate work
+			time.Sleep(500 * time.Millisecond)
+
+			log.Printf(
+				"finished job type=%s task_id=%d",
+				job.Type,
+				job.TaskID,
+			)
 		}
 	}()
 }
 
 // this sends a job into the channel
-func (p *Processor) Enqueue(job string) {
+func (p *Processor) Enqueue(job Job) {
 	p.jobs <- job
 }
 
